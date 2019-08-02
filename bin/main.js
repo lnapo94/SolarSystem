77155a72a1d56360ab36b7ca605c7c617dd45fa8
sirolf-otrebla <albeflor@gmail.com>
2019-08-02 12:00:55 +0200

    minor changes to code organization

--- bin/main.js
+++ bin/main.js
@@ -1,57 +1,65 @@
 import {Object} from "../data/planets.js";
+let then = 0;
 
-function drawScene() {
+function drawScene(then, deltatime) {
+    G_gl.viewport(0, 0, G_gl.canvas.width, G_gl.canvas.height);
     G_gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     G_gl.clearDepth(1.0);                 // Clear everything
     G_gl.enable(G_gl.DEPTH_TEST);           // Enable depth testing
     G_gl.depthFunc(G_gl.LEQUAL);            // Near things obscure far things
     G_gl.enable(G_gl.CULL_FACE);
+    G_gl.clear(G_gl.COLOR_BUFFER_BIT | G_gl.DEPTH_BUFFER_BIT);      // Clear the canvas before we start drawing on it.
 
-    // Enable the depth buffer
-    G_gl.enable(G_gl.DEPTH_TEST);
-
-    // Clear the canvas before we start drawing on it.
-    G_gl.clear(G_gl.COLOR_BUFFER_BIT | G_gl.DEPTH_BUFFER_BIT);
-
-    // Compute the matrices
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    // Compute perspective matrix
+    const fieldOfView = 60 * Math.PI / 180;   // in radians
     const aspect = G_gl.canvas.clientWidth / G_gl.canvas.clientHeight;
-    const zNear = 0.0001;
+    const zNear = 0.1;
     const zFar = 100.0;
     const projectionMatrix = mat4.create();
-    
     mat4.perspective(projectionMatrix,
         fieldOfView,
         aspect,
         zNear,
         zFar);
 
-    const modelViewMatrix = mat4.create();
-
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
-
-    mat4.translate(modelViewMatrix,     // destination matrix
-        modelViewMatrix,     // matrix to translate
-        [10.0, 10.0, -50.0]);  // amount to translate
-
-
     let o;
-    for (o of G_Objects){
+    for (o in G_Objects){
         G_gl.useProgram(G_program);
-        o.render(projectionMatrix, modelViewMatrix);
+        G_Objects[o].render(projectionMatrix);
     }
-    // Set the shader uniforms
+}
+
+function render(now) {
+    now *= 0.001;  // convert to seconds
+    const deltaTime = now - then;
+    then = now;
+
+    drawScene(then, deltaTime);
+
+    requestAnimationFrame(render);
+}
+
+function setModelViewMatrixes() {
+    let terraMVM = mat4.create();
+    console.log(terraMVM);
+    mat4.translate(terraMVM, terraMVM, [0, +.5,-2]);  // amount to translate
+    mat4.scale(terraMVM, terraMVM, [5,5,5]);
+    G_Objects.terra.setModelViewMatrix(terraMVM);
 }
 
 function  main(data) {
     let o;
     let res = loadMeshData(data);
-    console.log(res);
-    G_Objects.push( new Object(res, './data/assets/Terra1.jpg'));
-    for (o of G_Objects) { o.lookup(); }
-    requestAnimationFrame(drawScene)
+    G_Objects = {
+        terra : new Object(res, 'http://localhost:63342/SolarSystem/data/assets/Terra1.png?_ijt=o9keg75a007lrbem85al5r41sm'),
+    };
+    setModelViewMatrixes();
+    for (o in G_Objects) {
+        G_Objects[o].lookup();
+    }
+    requestAnimationFrame(render)
 }
+
 
 $.ajax({
     url: "./data/assets/terra1.obj",
