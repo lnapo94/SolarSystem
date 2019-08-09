617df3656f51fd542de369e702347dbe1295d482
lnapo94 <10464485@polimi.it>
2019-08-09 16:07:27 +0200

    Added error handling for heroku

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, './')));
 
+app.use(function(req, res) {
+    res.send("404 - Not Found");
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send("500 - Internal Server Error");
+});
+
 app.listen(SERVER_PORT, function () {
     console.log("The Server is now started");
 });
